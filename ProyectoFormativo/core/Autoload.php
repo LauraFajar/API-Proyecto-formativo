b734fd1f9ed885ef04e5463e2d7ea16732367648
Laura Fajardo <lauracamilafajardocalderon@gmail.com>
2025-03-10 20:47:51 -0500

    Delete ProyectoFormativo directory

--- ProyectoFormativo/core/Autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($nombre_clase) {
-    $ruta_clase = str_replace('\\', DIRECTORY_SEPARATOR, $nombre_clase) . '.php';
-    if (file_exists($ruta_clase)) {
-        require_once $ruta_clase;
-    }
-});
-?>
